e289bcc0e3abbb3bb612b0f01c6ff537f1668219
edwindeza <ejdeza@gmail.com>
2021-03-11 16:40:53 -0500

    first version

---
+++ src/components/GifGrid.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs';
+import { GifGridItem } from './GifGridItem';
+// import PropTypes from 'prop-types'
+
+function GifGrid({category}) {
+
+  const { data:images, loading } = useFetchGifs( category );
+  
+  return (
+    <>
+      <h3>{category}</h3>
+      { loading && <p>Loading</p>}
+      <div className='card-grid'>
+        {
+          images.map( img => ( 
+            <GifGridItem 
+                key={ img.id }
+                { ...img }
+            />
+        ))
+        }
+      </div>
+    </>
+  )
+}
+
+// GifGrid.propTypes = {
+
+// }
+
+export default GifGrid
+
